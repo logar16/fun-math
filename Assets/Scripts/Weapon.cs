7173d79cc44f287802527680ad4553bff1be0044
LuffaKing <141613628+LuffaKing@users.noreply.github.com>
2023-09-14 10:04:45 -0700

    Terechen/more levels (#17)
    * Add HUD
    
    * Add more levels with minor code refacoring

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -11,6 +11,19 @@ namespace FunMath
 
         void ShootArrow()
         {
+            // TODO: Use real-time setting when inventory is ready
+            //
+            //PlayerController player = gameObject.GetComponent<PlayerController>();
+            //OperationType operation = player.GetOperationSelector().QueryCurrentItem().Operator;
+            //int modifier = player.GetModifierSelector().QueryCurrentItem().Modifier;
+
+            OperationType operation = OperationType.Subtraction;
+            int modifier = 10;
+
+            Arrow arrow = arrowPrefab.GetComponent<Arrow>();
+            arrow.Operator = operation;
+            arrow.Modifier = modifier;
+
             Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
         }
     }
